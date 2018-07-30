9fdd92d73958ebbda865016f5d6700abccf2986f
bergi <s.berke@netzkolchose.de>
2018-07-31 00:16:18 +0200

    proper error handling

--- django_clear_tables/management/commands/django_clear_tables.py
+++ django_clear_tables/management/commands/django_clear_tables.py
@@ -1,7 +1,7 @@
 # encoding=utf-8
 
 from django.core.management.base import BaseCommand, CommandError
-
+from django.db.utils import OperationalError
 
 class Command(BaseCommand):
     help = 'DANGEROUS! Clear all django-tables after initial migrations to make loaddata work'
@@ -10,10 +10,24 @@ class Command(BaseCommand):
         clear_django_tables()
 
 
-
 def clear_django_tables():
+    was_error = False
+
     from django.contrib.auth.models import Permission
-    Permission.objects.all().delete()
+    try:
+        Permission.objects.all().delete()
+        print("Permission cleared")
+    except OperationalError:
+        print("No Permission table present")
+        was_error = True
 
     from django.contrib.contenttypes.models import ContentType
-    ContentType.objects.all().delete()
+    try:
+        ContentType.objects.all().delete()
+        print("ContentType cleared")
+    except OperationalError:
+        print("No ContentType table present")
+        was_error = True
+
+    if was_error:
+        print("Probably, you need to call ./manage.py migrate first")
